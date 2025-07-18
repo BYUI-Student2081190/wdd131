a0a600a4acce90db36bec9d63c337fec08cddde2
BYUI-Student2081190 <cscott1@byupathway.edu>
2025-07-17 18:47:13 -0600

    Completed CSS

--- scripts/place.js
+++ scripts/place.js
@@ -5,22 +5,19 @@ const temp = document.querySelector('#temp');
 const windSpeed = document.querySelector('#wind');
 const windChill = document.querySelector('#windChill');
 
-function calculateWindChill()
+function calculateWindChill(numTemp, numWindSpeed)
 {
-    // Convert the strings to numbers
-    const numTemp = Number(temp.textContent);
-    const numWindSpeed = Number(windSpeed.textContent);
-
-    // Calculate the wind chill
-    const calculatedWindChill = 35.74 + 0.6215 * numTemp - 35.75 * Math.pow(numWindSpeed, 0.16) + 0.4275 * numTemp * Math.pow(numWindSpeed, 0.16);
-    // Return the calculated wind chill
-    return `${Math.round(calculatedWindChill * 10) / 10} °F`;
+    return (35.74 + 0.6215 * numTemp - 35.75 * Math.pow(numWindSpeed, 0.16) + 0.4275 * numTemp * Math.pow(numWindSpeed, 0.16));
 };
 
 // Create an if statement to see if the wind chill should be calculated
 if (Number(temp.textContent) <= 50 && Number(windSpeed.textContent > 3))
 {
-    windChill.textContent = calculateWindChill();
+    // Convert the strings to numbers
+    const numTemp = Number(temp.textContent);
+    const numWindSpeed = Number(windSpeed.textContent);
+
+    windChill.textContent = `${Math.round(calculateWindChill(numTemp, numWindSpeed) * 10) / 10} °F`;
 } else {
     windChill.textContent = 'N/A';
 }
